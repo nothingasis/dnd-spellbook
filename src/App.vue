e25f00f20d9e720e12634ea2de23645eec834149
Nurthin Aziz <NurthinAziz@gmail.com>
2018-09-30 21:01:44 -0700

    Need to use python to web scrape all the spells from dungeons and dragons.

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,29 @@
       <v-container>
         <v-layout row wrap align-center>
 
-          <v-flex xs6 md5>
+          <v-flex xs12 md4>
             <div class="text-xs-center">
-              <v-avatar size="300" tile>
-                <img src="@/assets/wizard-hat.jpg" class="mb-5">
+              <v-avatar size="125px" tile>
+                <img src="@/assets/wizard-hat.png" class="mb-1">
               </v-avatar>
                 <div class="headline">Wizards <span style="font-weight:bold">Spellbook</span></div>
                 <div class="subheading text-xs-center grey--text pt-1 pb-3">may the dice roll in your favor</div>
-                <v-layout justify-space-between>
+                <v-layout justify-space-around>
                   <a href="javascript:;" class="body-2">Home</a>
                   <!-- TODO: Creating a settings component -->
                   <!-- <a href="javascript:;" class="body-2">Settings</a> -->
                   <a href="javascript:;" class="body-2">Github</a>
                 </v-layout>
             </div>
-            <v-layout>
+            <v-layout row wrap align-center>
               <spell-card :spell="spellSelected" :canCast="false"></spell-card>
             </v-layout>
           </v-flex>
 
-          <v-flex xs6 md5 offset-md2>
-            <spell-card v-for="(spell, key) in spellbook" :key="key" :spell="spell"></spell-card>
+          <v-flex xs12 md5 offset-md2>
+            <div v-for="spell in spellbook" :key="spell.title">
+              <spell-card :spell="spell"></spell-card>
+            </div>
           </v-flex>
         </v-layout>
       </v-container>
--- src/components/SpellCard.vue
+++ src/components/SpellCard.vue
@@ -1,49 +1,41 @@
 <template>
-  <!-- <div> -->
-  <!-- <v-container> -->
-  <!-- <v-layout row wrap align-center> -->
-
-  <!-- <v-flex xs6 md5> -->
-  <v-card class="my-3" hover width="475">
-    <v-container fluid>
-      <v-layout>
-        <v-flex xs12 align-end d-flex>
-          <span class="headline">{{ spell.title }}</span>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <v-card-title>
-      <v-layout>
-        <span class="grey--text">{{spell.type}}</span>
-      </v-layout>
-
-      <v-container>
-        <v-layout row wrap>
-          <v-flex xs6>
-            <b>Casting Time:</b>&nbsp;{{spell.castingtime}}
-          </v-flex>
-          <v-flex xs6>
-            <b>Range:</b>&nbsp;{{spell.range}}
-          </v-flex>
-          <v-flex xs6>
-            <b>Components:</b>&nbsp;{{spell.components}}
-          </v-flex>
-          <v-flex xs6>
-            <b>Duration:</b>&nbsp;{{spell.duration}}
+  <v-card class="my-3" hover width="100%">
+    <v-responsive class="white--text">
+      <v-container fill-height fluid>
+        <v-layout>
+          <v-flex xs12 align-end d-flex>
+            <span class="headline">{{ spell.title }}</span>
           </v-flex>
         </v-layout>
       </v-container>
+    </v-responsive>
+    <v-card-title>
+      {{ spell.type }}
     </v-card-title>
+    <v-card-text>
+      <v-layout row wrap>
+        <v-flex xs6>
+          <b>Casting Time:</b>&nbsp;{{spell.castingtime}}
+        </v-flex>
+        <v-flex xs6>
+          <b>Range:</b>&nbsp;{{spell.range}}
+        </v-flex>
+        <v-flex xs6>
+          <b>Components:</b>&nbsp;{{spell.components}}
+        </v-flex>
+        <v-flex xs6>
+          <b>Duration:</b>&nbsp;{{spell.duration}}
+        </v-flex>
+      </v-layout>
+    </v-card-text>
     <v-card-actions>
-      <v-btn color="success" v-if="canCast" @click="selectSpell(spell)">Cast a Spell</v-btn>
+      <v-btn flat class="red--text" @click="selectSpell(spell)">
+        Cast Spell
+      </v-btn>
       <v-spacer></v-spacer>
       <v-btn flat class="blue--text">Read More</v-btn>
     </v-card-actions>
   </v-card>
-  <!-- </v-flex> -->
-  <!-- </v-layout> -->
-  <!-- </v-container> -->
-  <!-- </div> -->
 </template>
 
 <script>
